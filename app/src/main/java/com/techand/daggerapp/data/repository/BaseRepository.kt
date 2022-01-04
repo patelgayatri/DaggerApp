b8db91d53aea4a726b0f1290b7de2e8da8529fc4
patelgayatri <gayatripatel566@gmail.com>
2022-01-04 13:01:58 +1100

    -Network check implementation

--- app/src/main/java/com/techand/daggerapp/data/repository/BaseRepository.kt
+++ app/src/main/java/com/techand/daggerapp/data/repository/BaseRepository.kt
@@ -1,8 +1,7 @@
 package com.techand.daggerapp.data.repository
 
-import com.techand.daggerapp.data.network.ApiService
 import com.techand.daggerapp.data.network.SafeApiCall
 
-abstract class BaseRepository(private val api: ApiService) : SafeApiCall {
+abstract class BaseRepository() : SafeApiCall {
 
 }
--- app/src/main/java/com/techand/daggerapp/data/repository/ImageRepository.kt
+++ app/src/main/java/com/techand/daggerapp/data/repository/ImageRepository.kt
@@ -4,7 +4,7 @@ import com.techand.daggerapp.data.network.ApiService
 import javax.inject.Inject
 
 
-class ImageRepository @Inject constructor(private val apiService: ApiService):BaseRepository(apiService)  {
+class ImageRepository @Inject constructor(private val apiService: ApiService):BaseRepository()  {
 
     suspend fun getImages() = safeApiCall {
         apiService.getImages()
